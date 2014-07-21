c7899eea086c52a8a3855790739a65cda11cb3dd
kamatama41 <shiketaudonko41@yahoo.co.jp>
2014-07-22 01:44:36 +0900

    Initial Commit

---
+++ src/main/java/com/kamatama41/netty/sample/http/HelloServerInitializer.java
@@ -0,0 +1,25 @@
+package com.kamatama41.netty.sample.http;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+public class HelloServerInitializer extends ChannelInitializer<SocketChannel> {
+    private ScheduledExecutorService service;
+
+    public HelloServerInitializer(ScheduledExecutorService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline p = ch.pipeline();
+        p.addLast("encoder", new HttpResponseEncoder());
+        p.addLast("decoder", new HttpRequestDecoder(4096, 8192, 8192, false));
+        p.addLast("handler", new HelloServerHandler(service));
+    }
+}
---
+++ src/main/java/com/kamatama41/netty/sample/http/Message.java
@@ -0,0 +1,14 @@
+package com.kamatama41.netty.sample.http;
+
+public class Message {
+    private final String message;
+
+    public Message(String message) {
+        super();
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
